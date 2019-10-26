d9dd78fd9f790a1f2bb0a84411ef90684435b630
leiclef <fibemini-freelance@memoad.jp>
2019-10-26 11:44:29 +0900

    jsファイルの修正

--- iroha/js/script.js
+++ iroha/js/script.js
@@ -1,18 +1,48 @@
 (function () {
   'use strict';
-  var $myImage = new Array(
-    "main-image-item1",
-    "main-image-item2",
-    "main-image-item3",
-    "main-image-item4",
-    "main-image-item5"
-  );
+  // humberger-menu-script
+  var $navTrigger = document.getElementById('nav-trigger');
+  var $navContents = document.getElementById('nav-contents');
 
-  function fadeIn(node, delaytimep) {
-    node.style.display = 'block';
-    node.style.zIndex = "1";
+  function navOpenFunc() {
+    $navContents.classList.add('nav-open');
+  }
+
+  function navCloseFunc() {
+    $navContents.classList.remove('nav-open');
+  }
+
+  $navTrigger.addEventListener('click', function () {
+    $navTrigger.classList.toggle('is-active');
+    if ($navContents.classList.contains('nav-open')) {
+      navCloseFunc();
+    } else {
+      navOpenFunc();
+    }
+  });
+
+  // slide-show-script
+  function fadeIn(node, duration) {
+    if (getComputedStyle(node).display !== 'none') return;
+
+    if (node.style.display === 'none') {
+      node.style.display = '';
+    } else {
+      node.style.display = 'block';
+    }
     node.style.opacity = 0;
-    setTimeout(node.style.opacity = 1, delaytimep);
+
+    var start = performance.now();
+
+    requestAnimationFrame(function tick(timestamp) {
+      var easing = (timestamp - start) / duration;
+      node.style.opacity = Math.min(easing, 1);
+      if (easing < 1) {
+        requestAnimationFrame(tick);
+      } else {
+        node.style.opacity = '';
+      }
+    });
   }
 
   function fadeOut(node, duration) {
@@ -30,25 +60,89 @@
     });
   }
 
-  var $nowCnt = 0;
-  var $nextCnt = 1;
-  function myChange() {
-    var $nowId = $myImage[$nowCnt];
-    var $nextId = $myImage[$nextCnt];
-    var $nowelem = document.getElementById($nowId);
-    var $nextelem = document.getElementById($nextId);
-    $nowelem.style.display = 'block';
-    $nowelem.style.zIndex = "3";
-    fadeIn($nextelem, 4000);
+  var $spImage = new Array(
+    "main-image-item11",
+    "main-image-item12",
+    "main-image-item13",
+    "main-image-item14",
+    "main-image-item15"
+  );
 
-    if ($nowelem.style.zIndex > $nextelem.style.zIndex) {
-      fadeOut($nowelem, 4000);
+  var $xsImage = new Array(
+    "main-image-item21",
+    "main-image-item22",
+    "main-image-item23",
+    "main-image-item24"
+  );
+  var $spnowCnt = -1;
+  var $spnextCnt = 1;
+  var $xsnowCnt = -1;
+  var $xsnextCnt = 1;
+  function spimageChange() {
+    if (window.matchMedia("(max-width: 410px)").matches) {
+      $spnowCnt = -1;
+      $spnextCnt = 1;
+      for (var i = 0; i < $spImage.length; i++) {
+        var $elem = document.getElementById($spImage[i])
+        if (i == 0) {
+          $elem.style.display = "block";
+        } else {
+          $elem.style.display = "none";
+        }
+      }
+    } else {
+      if ($spnowCnt < 0) {
+        $spnowCnt += 1
+      } else {
+        var $nowId = $spImage[$spnowCnt];
+        var $nextId = $spImage[$spnextCnt];
+        var $nowelem = document.getElementById($nowId);
+        var $nextelem = document.getElementById($nextId);
+
+        fadeIn($nextelem, 3000);
+
+        fadeOut($nowelem, 3000);
+
+
+        $spnowCnt = $spnextCnt;
+        $spnextCnt = ($spnextCnt + 1 < $spImage.length) ? $spnextCnt + 1 : 0;
+      }
     }
-
-    $nowCnt = $nextCnt;
-    $nextCnt = ($nextCnt + 1 < $myImage.length) ? $nextCnt + 1 : 0;
-
-    setTimeout(myChange, 6000)
+    setTimeout(spimageChange, 5000)
   }
-  myChange();
+  spimageChange();
+
+  function xsimageChange() {
+    if (window.matchMedia("(min-width: 411px)").matches) {
+      $xsnowCnt = -1;
+      $xsnextCnt = 1;
+      for (var i = 0; i < $xsImage.length; i++) {
+        var $elem = document.getElementById($xsImage[i])
+        if (i == 0) {
+          $elem.style.display = "block";
+        } else {
+          $elem.style.display = "none";
+        }
+      }
+    } else {
+      if ($xsnowCnt < 0) {
+        $xsnowCnt += 1
+      } else {
+        var $nowId = $xsImage[$xsnowCnt];
+        var $nextId = $xsImage[$xsnextCnt];
+        var $nowelem = document.getElementById($nowId);
+        var $nextelem = document.getElementById($nextId);
+
+        fadeIn($nextelem, 3000);
+
+        fadeOut($nowelem, 3000);
+
+
+        $xsnowCnt = $xsnextCnt;
+        $xsnextCnt = ($xsnextCnt + 1 < $xsImage.length) ? $xsnextCnt + 1 : 0;
+      }
+    }
+    setTimeout(xsimageChange, 5000)
+  }
+  xsimageChange();
 })();
